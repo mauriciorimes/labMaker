6eda6a32ba71afe69683f58a8a82fd9cd17dfe5e
mauriciorimes <mauriciorimes@gmail.com>
2023-05-04 08:27:25 -0300

    Tela adm responsiva e estilizada

--- src/pages/AdminAgendamentos/index.js
+++ src/pages/AdminAgendamentos/index.js
@@ -2,6 +2,7 @@ import TabelaAdmin from "components/TabelaAdmin";
 import { db } from "db/firebase";
 import { collection, getDocs } from "firebase/firestore/lite";
 import { useEffect, useState } from "react";
+import styles from './AdminAgendamentos.module.css';
 
 export default function AdminAgendamentos() {
 
@@ -13,22 +14,26 @@ export default function AdminAgendamentos() {
     useEffect(() => {
         const obterAgendamentos = async () => {
             const dataBD = await getDocs(useCollectionRef)
-            const todosAgendamentos = dataBD.docs.map((doc) => ({ ...doc.data(), id: doc.id }))          
-            const filtro = todosAgendamentos.filter(filtrados => filtrados.data === adminData)            
-            setAdminAgendamento(filtro)                      
+            const todosAgendamentos = dataBD.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
+            const filtro = todosAgendamentos.filter(filtrados => filtrados.data === adminData)
+            setAdminAgendamento(filtro)
         };
         obterAgendamentos();
-    }, [adminData]);    
+    }, [adminData]);
 
-    return(
-        <section>
-            <label> Selecione uma data para ver os agendamentos do dia: </label>
-            <input
-                value={adminData} 
-                type="date"
-                onChange={(e) => setAdminData(e.target.value)}
-            />            
-            <TabelaAdmin dados={adminAgendamento} data={adminData}/>
+    return (
+        <section className={styles.AdminAgendamentos}>
+            <h2> Área administrativa de agendamentos </h2>
+            <div>
+                <label> Selecione uma data para ver os agendamentos do dia: </label>
+                <input
+                    value={adminData}
+                    type="date"
+                    onChange={(e) => setAdminData(e.target.value)}
+                />
+            </div>
+            <h3> Tabela não disponível para versão mobile. </h3>            
+            <TabelaAdmin dados={adminAgendamento} data={adminData} />
         </section>
     )
 }
