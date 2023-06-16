c2dd5990291351a2fb77f3d0a1596e4b2a15f99b
mauriciorimes <mauriciorimes@gmail.com>
2023-06-16 12:04:27 -0300

    data id arte formatada br

--- src/components/IdArte/index.js
+++ src/components/IdArte/index.js
@@ -3,7 +3,11 @@ import people from 'image/id/people.png';
 
 export default function IdArte({ usuarioEncontrado, nome }) {
 
-    const nomeRecebido = [...new Set(nome.map(n => n.nome))]
+    const nomeRecebido = [...new Set(nome.map(n => n.nome))];
+
+    function reverseString(str) {
+        return str.split('-').reverse().join('/');
+    }
 
     return (
         <div>
@@ -18,7 +22,7 @@ export default function IdArte({ usuarioEncontrado, nome }) {
                 {usuarioEncontrado.map(usuario => (
                     <div>
                         <ul>
-                            <li> Data: {usuario.data} </li>
+                            <li> Data: {reverseString(usuario.data)} </li>
                             <li> Horário: de {usuario.horaInicial}:00 às {usuario.horaFinal}:00 </li>
                         </ul>
                     </div>
